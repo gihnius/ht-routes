ab0f24a5b1adabd6c85c8cab5b1fe0886519f1ea
gihnius <gihnius@gmail.com>
2014-09-13 00:04:18 +0800

    bug fixes and refactoring

--- ht-routes.lisp
+++ ht-routes.lisp
@@ -1,99 +1,114 @@
+;;;; ht-routes.lisp
+;;;;
+;;;; This file is part of the ht-routes library, released under MIT.
+;;;; See file README.org for details.
+;;;;
+;;;; Author: Gihnius lyj <gihnius@gmail.com>
+;;;;
+
 (defpackage :ht-routes
   (:use :cl :cl-ppcre :hunchentoot)
-  (:export :*route-params*
-           :map-routes))
+  (:export :map-routes
+           :*allow-head*
+           :ht-acceptor
+           :add-route))
 
 (in-package :ht-routes)
 
-(defvar *ht-routes* ())
+;; method: http request method (keyword)
+;; path: url def
+(defstruct route
+  method
+  path
+  path-scanner
+  handler)
 
-(defvar *route-params* ())
+;; list of routes struct
+(defvar *routes* ())
 
 (defparameter *route-param-scanner* (create-scanner "(:[^\\W]+)"))
 
 (defparameter *default-param-regex* "([^\/]+)")
 
-(defun get-param-names (path)
-  "return param names from path \"/obj/:param1/:param2\" etc."
-  (all-matches-as-strings *route-param-scanner* path))
+;; default allow :HEAD method
+(defvar *allow-head* t)
 
-(defun get-param-matches (route)
-  "return the param and match-regex pairs from route"
-  (let ((matches (mapcar #'read-from-string (get-param-names (car route))))
-        (dummy ())
-        (res ()))
-    ;; keep res store the params from matches in order
-    (dolist (m matches)
-      (push nil dummy))
-    (mapc #'(lambda (&rest e) (setq res (append res e))) matches dummy)
-    (loop
-       for p in matches
-       for v = (getf (cdr route) p)
-       do (setf (getf res p) v))
-    res))
-
-(defun get-method-handlers (route)
-  "return method and handler pairs"
-  (let ((res (cdr route))
-        (m (get-param-matches route)))
-    (dolist (e m)
-      (if (member e res :test #'equalp)
-          (setq res (remove e res))))
-    res))
-
-(defun make-path-scanner (route)
-  (let* ((path (car route))
-         (newpath path)
+;; make path matching regex
+(defun make-path-scanner (path params-matches)
+  (let* ((newpath path)
          (param-names (get-param-names path))
          ;; default pattern for any params
          (pattern *default-param-regex*))
     (if param-names
         (dolist (p param-names)
-          (let ((it (getf (cdr route) (read-from-string p))))
-            (when it (setf pattern (format nil "(~A)" it))))
-          (setf newpath (regex-replace p newpath pattern))))
+          (let ((it (getf params-matches (read-from-string p))))
+            (if it
+                (setf newpath (regex-replace p newpath (format nil "(~A)" it)))
+                (setf newpath (regex-replace p newpath pattern))))))
     (create-scanner (format nil "^~A$" newpath))))
 
-(defmacro map-routes (&body routes)
-  `(setq *ht-routes* (list ,@(loop for route in routes
-                                collect `(list (make-path-scanner ',route) ',route)))))
+;; add routes by template:
+;; (method "path" handler params-matches)
+;; (get "/books" show-all-books)
+;; (get "/books/:id" show-book :id "\\d+")
+;; (post "/books/update/:id" update-book :id "\\d+")
 
-(defun set-current-route-params (route values)
-  "set the match url params into *route-params*"
-  (let ((param-matches (get-param-matches route)))
-    (setf *route-params* param-matches)
-    (loop
-       for v in values
-       for (p r) on *route-params* by #'cddr
-       do (setf (getf *route-params* p) v))))
+(defmacro map-routes (&body templates)
+  `(setq *routes* (list ,@(loop for template in templates
+                                collect `(gen-route ',template)))))
+
+(defun gen-route (template)
+  (destructuring-bind (method path handler &rest params-matches) template
+    (make-route
+     :method (read-from-string (concatenate 'string ":" (string method)))
+     :path path
+     :path-scanner (make-path-scanner path params-matches)
+     :handler handler)))
+
+(defun add-route (template)
+  (push (gen-route template) *routes*))
+
+(defun get-param-names (path)
+  "return param names(':param1' ':param2') from path '/path/:param1/:param2' etc."
+  (all-matches-as-strings *route-param-scanner* path))
+
+(defun get-param-matches (path)
+  "return the param and match-regex pairs from route"
+  (let ((matches (mapcar #'read-from-string (get-param-names path)))
+        (dummy ())
+        (res ()))
+    (dolist (m matches)
+      (push nil dummy))
+    (mapc #'(lambda (&rest e) (setq res (append res e))) matches dummy)
+    res))
+
+(defun match-handler (route request)
+  (let ((params (get-param-matches (route-path route))))
+    (multiple-value-bind (match results)
+        (scan-to-strings (route-path-scanner route) (script-name request))
+      (when results
+        (loop
+           for v in (coerce results 'list)
+           for (p vv) on params by #'cddr
+           do (setf (getf params p) v)))
+      (if (or (and match *allow-head* (eq (request-method request) :HEAD))
+              (and match (eq (route-method route) (request-method request))))
+          (return-from match-handler (list (route-handler route) params))
+          (return-from match-handler (list nil nil))))))
+
+(defclass ht-acceptor (acceptor)
+  ())
 
 (defun defunction (func)
   (if (fboundp func)
       func
-      (setf (symbol-function func) #'(lambda()))))
+      #'(lambda()
+          (format nil "Handler not implemented!"))))
 
-;; override hunchentoot dispatcher by default
-;; define a new acceptor class is recommended here
-(defmethod acceptor-dispatch-request ((acceptor acceptor) request)
-  (if (null *ht-routes*)
-      (let ((path (and (acceptor-document-root acceptor)
-                       (request-pathname request))))
-        (if path
-            (handle-static-file
-             (merge-pathnames (if (equal "/" (script-name request)) #p"index.html" path)
-                              (acceptor-document-root acceptor)))))
-      (dolist (e *ht-routes*)
-        (let ((scanner (car e))
-              (route (cadr e)))
-          (multiple-value-bind (match results)
-              (scan-to-strings scanner (script-name*))
-            (when results
-              (set-current-route-params route (coerce results 'list)))
-            (when match
-              (return-from acceptor-dispatch-request
-                (let ((method (request-method*)))
-                  (loop for (route-method handler) on (get-method-handlers route) by #'cddr do
-                       (when (eq method route-method)
-                         (return-from acceptor-dispatch-request (funcall (defunction handler))))))))))))
-  (setf (return-code* *reply*) +http-not-found+)
-  (abort-request-handler))
+(defmethod acceptor-dispatch-request ((acceptor ht-acceptor) request)
+  (loop for route in *routes*
+     for (action params) = (match-handler route request)
+     when action return (if params
+                            (funcall (defunction action) params)
+                            (funcall (defunction action)))
+     finally (call-next-method)))
